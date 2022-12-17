43573e321a4b09f307017a92a072f1c31f36dff8
Annie-anu <96071910+Annie-anu@users.noreply.github.com>
2022-12-17 20:00:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const telegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
-const TOKEN = process.env.TOKEN;
-const bot = new telegramBot(TOKEN, {polling : true});
-bot.on('message',(message) => {
-    console.log(message);
-})
+var TelegramBot = require('node-telegram-bot-api');
+var token = '5969452356:AAFrpSRa1toVAb9rMfC-Dkots1V0Vo-xQeo';
+
+const bot = new TelegramBot(token, {polling:true});
+bot.onText('message',function(msg) {
+
+var text = msg.text;
+var chatId = msg.chat.id;
+var type = msg.chat.type;
+var userid = msg.from.id;
+
+if(text == "id"){
+bot.sendMessage(chatId+"chtid");
+
+}
+});
